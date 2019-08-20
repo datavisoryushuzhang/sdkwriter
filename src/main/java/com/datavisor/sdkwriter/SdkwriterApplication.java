3b6335d5f80f2058808b5719c14c6db76e6838db
datavisoryushuzhang <yushu.zhang@datavisor.com>
2019-08-20 13:52:08 +0800

    initialize application by spring

---
+++ src/main/java/com/datavisor/sdkwriter/SdkwriterApplication.java
@@ -0,0 +1,13 @@
+package com.datavisor.sdkwriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdkwriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdkwriterApplication.class, args);
+	}
+
+}
